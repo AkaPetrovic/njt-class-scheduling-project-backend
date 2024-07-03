156c5204b28b11e73d05ce1bcd23bf85762c4bea
AkaPetrovic <akapetrovic@gmail.com>
2024-07-03 22:15:42 +0200

    Project created with initial structure and settings

---
+++ src/test/java/rs/ac/bg/fon/njt/NJT/Project/NjtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.njt.NJT.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
