fc53e7ff617142a865fed26208937f29e052a3ab
AkaPetrovic <akapetrovic@gmail.com>
2024-08-15 21:48:26 +0200

    Added methods CRUD operations for ClassCoveragePlan

--- src/main/java/rs/ac/bg/fon/njt/classCoveragePlan/ClassCoveragePlanController.java
+++ src/main/java/rs/ac/bg/fon/njt/classCoveragePlan/ClassCoveragePlanController.java
@@ -1,7 +1,10 @@
 package rs.ac.bg.fon.njt.classCoveragePlan;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -10,5 +13,50 @@ public class ClassCoveragePlanController {
 
     public ClassCoveragePlanController(ClassCoveragePlanService classCoveragePlanService) {
         this.classCoveragePlanService = classCoveragePlanService;
+    }
+
+    @GetMapping(path = "/class-coverage-plans")
+    public List<ClassCoveragePlan> getAllClassCoveragePlans() {
+        return classCoveragePlanService.getAllClassCoveragePlans();
+    }
+
+    @GetMapping(path = "/class-coverage-plans/{id}")
+    public ClassCoveragePlan getClassCoveragePlanById(@PathVariable Long id) {
+        return classCoveragePlanService.getClassCoveragePlanById(id);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PutMapping(path = "/class-coverage-plans/edit")
+    public ResponseEntity<String> updateClassCoveragePlan(@RequestBody ClassCoveragePlan classCoveragePlan) {
+        System.out.println(classCoveragePlan);
+//        return new ResponseEntity<>("Success: Class coverage plan edited successfully.", HttpStatus.OK);
+        ClassCoveragePlan updatedClassCoveragePlan = classCoveragePlanService.updateClassCoveragePlan(classCoveragePlan);
+        if (updatedClassCoveragePlan != null) {
+            return new ResponseEntity<>("Success: Class coverage plan updated successfully.", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Error: Class coverage plan update failed.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping(path = "/class-coverage-plans/add")
+    public ResponseEntity<String> addClassCoveragePlan(@RequestBody ClassCoveragePlan classCoveragePlan) {
+        System.out.println(classCoveragePlan);
+//        return new ResponseEntity<>("Success: Class coverage plan added successfully.", HttpStatus.OK);
+        ClassCoveragePlan addedClassCoveragePlan = classCoveragePlanService.addClassCoveragePlan(classCoveragePlan);
+        if (addedClassCoveragePlan != null) {
+            return new ResponseEntity<>("Success: Class coverage plan added successfully.", HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>("Error: Class coverage plan has not been added successfully.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping(path = "/class-coverage-plans/delete")
+    public ResponseEntity<String> deleteClassCoveragePlan(@RequestBody ClassCoveragePlan classCoveragePlan) {
+        System.out.println(classCoveragePlan);
+//        return new ResponseEntity<>("Success: Class coverage plan deleted successfully.", HttpStatus.OK);
+        classCoveragePlanService.deleteClassCoveragePlan(classCoveragePlan);
+        return new ResponseEntity<>("Success: Class coverage plan deleted successfully.", HttpStatus.OK);
     }
 }
--- src/main/java/rs/ac/bg/fon/njt/classCoveragePlan/ClassCoveragePlanService.java
+++ src/main/java/rs/ac/bg/fon/njt/classCoveragePlan/ClassCoveragePlanService.java
@@ -2,11 +2,39 @@ package rs.ac.bg.fon.njt.classCoveragePlan;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClassCoveragePlanService {
     private final ClassCoveragePlanRepository classCoveragePlanRepository;
 
     public ClassCoveragePlanService(ClassCoveragePlanRepository classCoveragePlanRepository) {
         this.classCoveragePlanRepository = classCoveragePlanRepository;
+    }
+
+    public List<ClassCoveragePlan> getAllClassCoveragePlans() {
+        return classCoveragePlanRepository.findAll();
+    }
+
+    public ClassCoveragePlan getClassCoveragePlanById(Long id) {
+        Optional<ClassCoveragePlan> optionalClassCoveragePlan = classCoveragePlanRepository.findById(id);
+        if(optionalClassCoveragePlan.isPresent()) {
+            return optionalClassCoveragePlan.get();
+        }else{
+            throw new IllegalArgumentException("There is no class coverage plan with the given ID: " + id);
+        }
+    }
+
+    public ClassCoveragePlan updateClassCoveragePlan(ClassCoveragePlan classCoveragePlan) {
+        return classCoveragePlanRepository.save(classCoveragePlan);
+    }
+
+    public ClassCoveragePlan addClassCoveragePlan(ClassCoveragePlan classCoveragePlan) {
+        return classCoveragePlanRepository.save(classCoveragePlan);
+    }
+
+    public void deleteClassCoveragePlan(ClassCoveragePlan classCoveragePlan) {
+        classCoveragePlanRepository.deleteById(classCoveragePlan.getId());
     }
 }
